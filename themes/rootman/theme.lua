170ff1f7cce632fce61ec83128a1fcf7c153de3a
Roman Gerhardt <rootman@virtualkitchen.org>
2012-05-23 13:32:05 +0200

    enabled wallpaper

--- themes/rootman/theme.lua
+++ themes/rootman/theme.lua
@@ -79,7 +79,8 @@ theme.titlebar_maximized_button_normal_active = "/usr/share/awesome/themes/defau
 theme.titlebar_maximized_button_focus_active  = "/usr/share/awesome/themes/default/titlebar/maximized_focus_active.png"
 
 -- You can use your own command to set your wallpaper
---  theme.wallpaper_cmd = { "awsetbg -f -R /usr/share/wallpapers/1280&" }
+-- theme.wallpaper_cmd = { "awsetbg -f -R /usr/share/wallpapers/1280&" }
+ theme.wallpaper_cmd = { "awsetbg -f /usr/share/archlinux/wallpaper/archlinux-simplyblack-43.png" }
 
 -- You can use your own layout icons like this:
 theme.layout_fairh = "/usr/share/awesome/themes/default/layouts/fairhw.png"
@@ -95,7 +96,8 @@ theme.layout_tiletop = "/usr/share/awesome/themes/default/layouts/tiletopw.png"
 theme.layout_spiral  = "/usr/share/awesome/themes/default/layouts/spiralw.png"
 theme.layout_dwindle = "/usr/share/awesome/themes/default/layouts/dwindlew.png"
 
-theme.awesome_icon = "/usr/share/awesome/icons/awesome16.png"
+theme.awesome_icon = "/usr/share/archlinux/wmlogos/archlinux-wm-awesome.png"
+--theme.awesome_icon = "/usr/share/awesome/icons/awesome16.png"
 
 return theme
 -- vim: filetype=lua:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:encoding=utf-8:textwidth=80
